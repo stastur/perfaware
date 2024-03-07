66aa66c7473414a10d67335bf938a145a0deb62f
Stanislau Tur <stanislau.tur@gmail.com>
2024-03-07 22:55:25 +0100

    IR for instruction

--- sim8086/main.go
+++ sim8086/main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type BitsType int
@@ -116,22 +117,25 @@ func main() {
 	}
 
 	fmt.Println("bits 16")
-	index := 0
-	for {
-		index, err = DecodeInstruction(index, buff)
+
+	for index := 0; index < len(buff); {
+		instruction, err := DecodeInstruction(index, buff)
 		if err != nil {
 			fmt.Println(";", err)
 			break
 		}
+
+		fmt.Println(instruction.String())
+		index += instruction.Size
 	}
 }
 
-func DecodeInstruction(startingAt int, buff []byte) (int, error) {
-	currentByteIndex := startingAt
-
-	if currentByteIndex >= len(buff) {
-		return -1, errors.New("EOF")
+func DecodeInstruction(startingAt int, buff []byte) (*Instruction, error) {
+	if startingAt >= len(buff) {
+		return nil, errors.New("EOF")
 	}
+
+	currentByteIndex := startingAt
 
 	for _, bp := range Blueprints {
 		var bitsSet uint32
@@ -215,55 +219,40 @@ func DecodeInstruction(startingAt int, buff []byte) (int, error) {
 		bits[Bits_HasDisp] = readFromBuff(hasDisp, mod == 0b10 || hasDirectAddress, w)
 		bits[Bits_HasData] = readFromBuff(hasData, w && !s, s)
 
-		var operandLeft interface{}
+		var instruction Instruction
+
 		if isTypeSet(bitsSet, Bits_Mod) {
-			operandLeft = DecodeMemory(rm, mod, w, bits[Bits_HasDisp])
+			instruction.Operands[0] = DecodeRm(rm, mod, w, bits[Bits_HasDisp])
 		} else if isTypeSet(bitsSet, Bits_HasAddr) {
-			operandLeft = fmt.Sprintf("[%d]", readFromBuff(true, w, false))
+			instruction.Operands[0] = DirectAddress(readFromBuff(true, w, false))
 		}
 
-		var operandRight interface{}
 		if isTypeSet(bitsSet, Bits_Reg) {
-			operandRight = DecodeReg(bits[Bits_Reg], w)
+			instruction.Operands[1] = DecodeReg(bits[Bits_Reg], w)
 		}
 
 		if bits[Bits_D] == 1 || (isTypeSet(bitsSet, Bits_E) && bits[Bits_E] == 0) {
-			operandLeft, operandRight = operandRight, operandLeft
+			instruction.Operands[0], instruction.Operands[1] = instruction.Operands[1], instruction.Operands[0]
 		}
 
 		if isTypeSet(bitsSet, Bits_HasData) {
-			var size string
-			if w {
-				size = "word"
-			} else {
-				size = "byte"
-			}
-
-			operandRight = fmt.Sprintf("%s %d", size, bits[Bits_HasData])
+			instruction.Operands[1] = Immediate{bits[Bits_HasData], w}
 		}
 
-		fmt.Print(bp.Name)
-		if operandLeft != nil {
-			fmt.Printf(" %s,", operandLeft)
-		}
+		instruction.Op = bp.Name
+		instruction.Size = bytesRead
 
-		if operandRight != nil {
-			fmt.Printf(" %s\n", operandRight)
-		}
-
-		currentByteIndex += bytesRead
-
-		return currentByteIndex, nil
+		return &instruction, nil
 	}
 
-	return -1, errors.New("No command")
+	return nil, errors.New("No command")
 }
 
-func DecodeMemory(rm uint16, mod uint16, wide bool, disp uint16) interface{} {
+func DecodeRm(rm uint16, mod uint16, wide bool, disp uint16) Operand {
 	switch mod {
 	case 0b00:
 		if rm == 0b110 {
-			return fmt.Sprintf("[%d]", disp)
+			return DirectAddress(disp)
 		}
 		return eac(rm, mod, wide, disp)
 
@@ -277,10 +266,10 @@ func DecodeMemory(rm uint16, mod uint16, wide bool, disp uint16) interface{} {
 		return DecodeReg(rm, wide)
 	}
 
-	return ""
+	return nil
 }
 
-func eac(rm uint16, mod uint16, wide bool, disp uint16) string {
+func eac(rm uint16, mod uint16, wide bool, disp uint16) EffectiveAddress {
 	regs := []string{
 		"bx+si",
 		"bx+di",
@@ -292,11 +281,11 @@ func eac(rm uint16, mod uint16, wide bool, disp uint16) string {
 		"bx",
 	}
 
-	return fmt.Sprintf("[%s%+d]", regs[rm], int16(disp))
+	return EffectiveAddress{regs[rm], int16(disp)}
 }
 
-func DecodeReg(reg uint16, wide bool) string {
-	regs := [][2]string{
+func DecodeReg(reg uint16, wide bool) Register {
+	regs := [][2]Register{
 		{"al", "ax"},
 		{"cl", "cx"},
 		{"dl", "dx"},
@@ -318,4 +307,60 @@ func DecodeReg(reg uint16, wide bool) string {
 func isTypeSet(flags uint32, bitsType BitsType) bool {
 	bit := uint32(1 << bitsType)
 	return flags&bit == bit
+}
+
+type Operand interface {
+	fmt.Stringer
+}
+
+type Register string
+
+func (reg Register) String() string {
+	return string(reg)
+}
+
+type Immediate struct {
+	Value uint16
+	Wide  bool
+}
+
+func (imm Immediate) String() string {
+	size := "byte"
+	if imm.Wide {
+		size = "word"
+	}
+
+	return fmt.Sprintf("%s %d", size, imm.Value)
+}
+
+type DirectAddress int
+
+func (addr DirectAddress) String() string {
+	return fmt.Sprintf("[%d]", addr)
+}
+
+type EffectiveAddress struct {
+	Base string
+	Disp int16
+}
+
+func (ea EffectiveAddress) String() string {
+	return fmt.Sprintf("[%s%+d]", ea.Base, ea.Disp)
+}
+
+type Instruction struct {
+	Op       string
+	Size     int
+	Operands [2]Operand
+}
+
+func (inst Instruction) String() string {
+	var stringOperands []string
+	for _, op := range inst.Operands {
+		if op != nil {
+			stringOperands = append(stringOperands, op.String())
+		}
+	}
+
+	return fmt.Sprintf("%s %s", inst.Op, strings.Join(stringOperands, ", "))
 }
